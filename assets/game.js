f25f93e3a4951a79a08e173efc498c0b9da4e1a0
Phong <ngphong.0708@gmail.com>
2023-03-26 22:50:39 +0700

    first commit

---
+++ assets/game.js
@@ -0,0 +1,16 @@
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
+
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
